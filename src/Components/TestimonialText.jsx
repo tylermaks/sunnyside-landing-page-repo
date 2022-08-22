153b0a26fc447ea71789f2b712c8efab9cb9d107
Tyler Maksymiw <tylermcodes@gmail.com>
2022-08-22 09:10:38 -0400

    Update testimonial img alt property

--- src/Components/TestimonialText.jsx
+++ src/Components/TestimonialText.jsx
@@ -4,7 +4,7 @@ import React from "react"
 function TestimonialText(props){ 
     return(
         <div className="testimonial-text-container">
-            <img className="testimonial-img" src={props.photo} alt="Testimonial Photo" />
+            <img className="testimonial-img" src={props.photo} alt="Testimonial" />
             <p className="testimonial-text">{props.testimonial}</p>
             <h3>{props.name}</h3>
             <span>{props.role}</span>
