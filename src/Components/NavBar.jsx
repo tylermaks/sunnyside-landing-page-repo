a8a93216bf19b5c257a1bf45ef49b39b59f1d843
Tyler Maksymiw <tylermcodes@gmail.com>
2022-08-21 21:32:18 -0400

    Add mobile menu and update responsive css

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,14 +1,29 @@
-import React from "react"
+import React, {useState} from "react"
 import "../Assets/Styles/NavBar/NavBar.css"
 
 import logo from "../Assets/images/logo.svg"
 import hamburger from "../Assets/images/icon-hamburger.svg"
 
 function NavBar(){
+
+    const [menu, setMenu] = useState(false)
+    const clicked = menu ? "menu menu--active" : "menu"
+
+    const handleClick = (e) => { 
+        setMenu(!menu)
+    }
+
+
     return(
         <nav id="nav-bar">
             <img src={logo} alt="Logo" />
-            <img className="hamburger-menu" src={hamburger} alt="Menu" />
+            <img onClick={handleClick} className="mobile-icon" src={hamburger} alt="Menu" />
+            <div className={clicked}>
+                <span className="nav-link">About</span>
+                <span className="nav-link">Services</span>
+                <span className="nav-link">Projects</span>
+                <button className="contact-btn">Contact</button>
+            </div>
         </nav>
     )
 }
